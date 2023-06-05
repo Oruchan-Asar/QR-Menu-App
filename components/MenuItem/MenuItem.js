08b00f44295c9a5a7840a1451dabc9f13ee4eb7a
Oruchan Asar <oruchan.asar@gmail.com>
2023-06-05 19:53:06 +0300

    Edit menu items

--- components/MenuItem/MenuItem.js
+++ components/MenuItem/MenuItem.js
@@ -1,8 +1,16 @@
+"use client";
+
 import styles from "./menuItem.module.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 export default function MenuItem({ name, price }) {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
-    <div className={styles.menu_item}>
+    <div className={styles.menu_item} data-aos="fade-up">
       <span>{name}</span>
       <p>{price}TL</p>
     </div>
