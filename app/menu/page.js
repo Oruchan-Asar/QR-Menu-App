f3444c5f563d5a8f7ca04fec288ffaa2df756add
Oruchan Asar <oruchan.asar@gmail.com>
2023-06-07 15:41:02 +0300

    change filter function in Menu page

--- app/menu/page.js
+++ app/menu/page.js
@@ -4,7 +4,7 @@ import MenuItem from "@/components/MenuItem/MenuItem";
 import styles from "./page.module.css";
 import localFont from "next/font/local";
 import menu from "@/mock/menu.js";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import dis_mekan from "@/public/place/dis-mekan.png";
 
@@ -12,69 +12,59 @@ const raleway = localFont({ src: "../Raleway.ttf" });
 
 export default function Menu() {
   const [filter, setFilter] = useState(null);
+  const [filteredMenu, setFilteredMenu] = useState(menu);
+
+  useEffect(() => {
+    if (filter) {
+      const foundMenu = menu.find((item) => item.id === filter);
+      setFilteredMenu(foundMenu ? [foundMenu] : menu);
+    } else {
+      setFilteredMenu(menu);
+    }
+  }, [filter]);
 
   return (
     <div className={styles.menu}>
       <div className={styles.banner}>
         <div className={styles.absolute}>
-          <h1 className={styles.menuTitle}>Menu</h1>
+          <h1 className={styles.menuTitle}>Menü</h1>
         </div>
         <Image className={styles.image} src={dis_mekan} alt="dis_mekan" />
       </div>
       <div className={styles.filter}>
-        {menu.map((item) => (
-          <ul key={item.id}>
-            <li>
-              <button
-                className={[
-                  `${styles.button} ${filter === item.id ? styles.active : ""}`,
-                ]}
-                onClick={() => setFilter(item.id)}
-              >
-                {item.categoryName}
-              </button>
-            </li>
-          </ul>
+        {menu.map((item, index) => (
+          <button
+            className={[
+              `${styles.button} ${filter === item.id ? styles.active : ""}`,
+            ]}
+            onClick={() => setFilter(item.id)}
+            key={index}
+          >
+            {item.categoryName}
+          </button>
         ))}
       </div>
       {filter && (
-        <button className={styles.cancel} onClick={() => setFilter()}>
+        <button className={styles.cancel} onClick={() => setFilter(null)}>
           Filtreyi temizle
         </button>
       )}
       <div className="align-center">
         <div className={styles.container}>
-          {filter
-            ? menu
-                .filter((item) => item.id === filter)
-                .map((item) => (
-                  <>
-                    <h2 className={[`${styles.title} ${raleway.className}`]}>
-                      {item.categoryName}
-                    </h2>
-                    <ul className={styles.menus}>
-                      {item.products.map((product) => (
-                        <li key={product.id}>
-                          <MenuItem name={product.name} price={product.price} />
-                        </li>
-                      ))}
-                    </ul>
-                  </>
-                ))
-            : menu.map((item) => (
-                <>
-                  <h2 className={[`${styles.title} ${raleway.className}`]}>
-                    {item.categoryName}
-                  </h2>
-                  <ul className={styles.menus}>
-                    {item.products.map((product) => (
-                      <li key={product.id}>
-                        <MenuItem name={product.name} price={product.price} />
-                      </li>
-                    ))}
-                  </ul>
-                </>
-              ))}
+          {filteredMenu.map((item, index) => (
+            <div key={index}>
+              <h2 className={[`${styles.title} ${raleway.className}`]}>
+                {item.categoryName}
+              </h2>
+              <ul className={styles.menus}>
+                {item.products.map((product, index) => (
+                  <li key={index}>
+                    <MenuItem name={product.name} price={product.price} />
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </div>
       </div>
     </div>
