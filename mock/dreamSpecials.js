dafbe03f8a23b913f51a33f0df74d2f62b05f236
Oruchan Asar <oruchan.asar@gmail.com>
2023-06-10 22:32:51 +0300

    move dreamSpecials location

--- mock/dreamSpecials.js
+++ mock/dreamSpecials.js
@@ -1,12 +1,12 @@
-import drink1 from "@/public/dreamSpecials/drink1.jpg";
-import drink2 from "@/public/dreamSpecials/drink2.jpg";
-import drink3 from "@/public/dreamSpecials/drink3.jpg";
-import drink4 from "@/public/dreamSpecials/drink4.jpg";
-import drink5 from "@/public/dreamSpecials/drink5.jpg";
-import drink6 from "@/public/dreamSpecials/drink6.jpg";
-import drink7 from "@/public/dreamSpecials/drink7.jpg";
-import sufle from "@/public/dreamSpecials/sufle.jpg";
-import dondurma from "@/public/dreamSpecials/dondurma.jpg";
+import drink1 from "@/app/assets/dreamSpecials/drink1.jpg";
+import drink2 from "@/app/assets/dreamSpecials/drink2.jpg";
+import drink3 from "@/app/assets/dreamSpecials/drink3.jpg";
+import drink4 from "@/app/assets/dreamSpecials/drink4.jpg";
+import drink5 from "@/app/assets/dreamSpecials/drink5.jpg";
+import drink6 from "@/app/assets/dreamSpecials/drink6.jpg";
+import drink7 from "@/app/assets/dreamSpecials/drink7.jpg";
+import sufle from "@/app/assets/dreamSpecials/sufle.jpg";
+import dondurma from "@/app/assets/dreamSpecials/dondurma.jpg";
 
 const dreamSpecials = [
   drink1,
