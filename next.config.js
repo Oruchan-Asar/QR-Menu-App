55ea1a173f7668262e64d4b07ca357064ff931fb
Oruchan Asar <oruchan.asar@gmail.com>
2024-03-09 22:20:45 +0300

    Remove redirects and add new redirect in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // redirect this page to https://www.coffeedream.com.tr/ this url
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "https://www.coffeedream.com.tr/",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
