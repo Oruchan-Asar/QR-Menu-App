43dd0ca7b7c176e178159c4d69c859fd940ffa70
Oruchan Asar <oruchan.asar@gmail.com>
2024-03-09 23:30:13 +0300

    Add redirect rule and update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // redirect this page to https://www.coffeedream.com.tr/ this url
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "https://www.coffeedream.com.tr/",
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
